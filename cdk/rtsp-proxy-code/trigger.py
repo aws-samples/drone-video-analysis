c388f7acfe55937b8144f173afc8120eb5c9a179
ishan-aws <85854534+ishan-aws@users.noreply.github.com>
2023-03-09 12:15:08 +1100

    fix: security finding to parse shell input
    fix parsing of shell input before command

--- cdk/rtsp-proxy-code/trigger.py
+++ cdk/rtsp-proxy-code/trigger.py
@@ -2,6 +2,8 @@
 
 from flask import Flask, request, jsonify
 import os
+from shlex import join, quote, split
+
 
 app = Flask(__name__)
 
@@ -13,9 +15,9 @@ def trigger():
     # Basically run the command ./kvs_gstreamer_sample teststream rtsp://localhost:8554/mystream
     # in the terminal as a subprocess
     # and then return the url
-    kinesis_stream_name = request.json.get("kinesis_stream_name", "teststream")
-    rtsp_src = request.json.get("rtsp_src", "rtsp://rtspserver:8554/mystream")
-    command = f"./kvs_gstreamer_sample {kinesis_stream_name} {rtsp_src} &"
+    kinesis_stream_name = join(split(request.json.get("kinesis_stream_name", "teststream")))
+    rtsp_src = join(split(request.json.get("rtsp_src", "rtsp://rtspserver:8554/mystream")))
+    command = f"./kvs_gstreamer_sample {} {} &".format(quote(kinesis_stream_name), quote(rtsp_src))
     os.system(command)
     return jsonify({"message": "success"})
 
